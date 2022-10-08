59921c98f5aa2b511a0859fd9de8fc07edb4c2d1
Ricardo Machado <ricardo.machado7@hotmail.com>
2022-10-08 20:29:07 -0300

    Criadas as Bibliotecas de Extração do Link e Nome

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 import fs from 'fs';
-import chalk from "chalk";
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados;
+}
 
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'Não encontramos o arquivo'));
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
-// async/awair
-async function pegaArquivo(caminhoDoArquivo){
-    try {
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
     const encoding = 'utf-8';
-    const texto = await fs.promises.readFile(caminhoDoArquivo , encoding)
-    console.log(chalk.green(texto));
-    } catch (erro){
-        trataErro(erro)
-    }
-    
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
-
-
-
-// Promises com then()
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch(trataErro)
-// }
 
 pegaArquivo('./arquivos/texto.md');
-
