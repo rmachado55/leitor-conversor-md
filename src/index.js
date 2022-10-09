0f2574453f721e5cf4f86cd03a1f9f92a95acc4f
Ricardo Machado <ricardo.machado7@hotmail.com>
2022-10-09 13:00:42 -0300

    Add função de ler aquivo ou caminho

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
   const capturas = [...texto.matchAll(regex)];
   const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-  return resultados;
+  return resultados.length !== 0 ? resultados : "Não há links no Arquivo";
 }
 
 function trataErro(erro) {
@@ -19,10 +19,10 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro)
   }
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
